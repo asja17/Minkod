e8059bcfe89ada22f364309855b290b5d2404b0c
Asim Jarrar <asim@kth.se>
2024-01-01 17:40:02 +0100

    första

---
+++ test3.py
@@ -0,0 +1,15 @@
+import csv
+with open('department.csv', newline='') as csvfile:
+ data = csv.DictReader(csvfile)
+ svensklista = []
+ print("ID Department Name")
+ print("---------------------------------")
+ for row in data:
+   svensklista.append(row['department_name'])
+
+for item in svensklista:
+  print (item)
+
+
+
+   
